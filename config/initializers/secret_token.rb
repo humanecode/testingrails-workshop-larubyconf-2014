8e5811112d185eadd31751de549e618ffafc3580
Mike Moore <mike@blowmage.com>
2014-02-06 04:56:36 -0800

    Empty rails app
    $ rails new standup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Standup::Application.config.secret_key_base = '4c611346704e8da631a8847c79d7d52bb5ae77849a1cc76edaba7003e9a7f64d8a37c2abd1e7681375e17d2181aa3b712e71fe29fa0f69f8832a1e7834f0c644'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Standup::Application.config.session_store :cookie_store, key: '_standup_session'
