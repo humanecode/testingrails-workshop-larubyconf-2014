566d58767c454ca0480341fe2bf12a72c1cf5147
Mike Moore <mike@blowmage.com>
2014-02-06 14:44:50 -0800

    Add Membership is invalid without unique group/user test

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+
+  validates :group, presence: true
+  validates :user, presence: true
 end
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class MembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def group
+    @group ||= groups :dev
+  end
+
+  def user
+    @user ||= users :nonmember
+  end
+
+  def membership
+    @membership ||= begin
+      m = Membership.new
+      m.group = group
+      m.user = user
+      m
+    end
+  end
+
+  def test_can_be_valid
+    assert membership.valid?
+  end
+
+  def test_is_invalid_without_group
+    membership.group = nil
+    refute membership.valid?
+    assert_includes membership.errors[:group], "can't be blank"
+  end
+
+  def test_is_invalid_without_user
+    membership.user = nil
+    refute membership.valid?
+    assert_includes membership.errors[:user], "can't be blank"
+  end
 end
