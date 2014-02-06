f923c04111d2bf345aa6f2ae48141fc0ecab049a
Mike Moore <mike@blowmage.com>
2014-02-06 06:03:02 -0800

    Update create one record test

--- test/models/user/create_test.rb
+++ test/models/user/create_test.rb
@@ -3,10 +3,14 @@ require 'test_helper'
 class User::CreateTest < ActiveSupport::TestCase
   def test_create_1_record
     begin_count = User.count
+    create_random_user
+    assert_equal begin_count + 1, User.count
+  end
+
+  def create_random_user
     random = SecureRandom.hex(8)
     User.create! email: "#{random}@example.com",
                  password: random,
                  password_confirmation: random
-    assert_equal begin_count + 1, User.count
   end
 end
