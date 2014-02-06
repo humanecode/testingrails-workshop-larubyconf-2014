04124c297637002781d96a7907aebe15a419c219
Mike Moore <mike@blowmage.com>
2014-02-06 15:02:02 -0800

    Post is invalid without unique group/user/date and body tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
+
+  validates :group_id, presence: true
+  validates :user_id, presence: true,
+                      uniqueness: { scope: [:group_id, :date] }
+  validates :date, presence: true
+  validates :body, presence: true
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,71 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def group
+    @group ||= groups :dev
+  end
+
+  def user
+    @user ||= users :member
+  end
+
+  def day
+    Date.today
+  end
+
+  def post
+    @post ||= begin
+      p = Post.new
+      p.group = group
+      p.user = user
+      p.date = day
+      p.body = "Great day!"
+      p
+    end
+  end
+
+  def test_can_be_valid
+    assert post.valid?
+  end
+
+  def test_is_invalid_without_group
+    post.group = nil
+    refute post.valid?
+    assert_includes post.errors[:group_id], "can't be blank"
+  end
+
+  def test_is_invalid_without_user
+    post.user = nil
+    refute post.valid?
+    assert_includes post.errors[:user_id], "can't be blank"
+  end
+
+  def test_is_invalid_without_day
+    post.date = nil
+    refute post.valid?
+    assert_includes post.errors[:date], "can't be blank"
+  end
+
+  def test_is_invalid_without_day
+    post.body = nil
+    refute post.valid?
+    assert_includes post.errors[:body], "can't be blank"
+  end
+
+  def test_invalid_with_duplicate_group_user_date
+    post = posts :today
+    post2 = Post.new
+    post2.group = post.group
+    post2.user  = post.user
+    post2.date  = post.date
+    post2.body  = post.body
+    refute post2.valid?
+    assert_includes post2.errors[:user_id], "has already been taken"
+  end
 end
+
+
+
+
+
+
