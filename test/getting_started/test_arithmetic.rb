a5dad02b321ece7356e98dffe10bcc2b8c26be8e
Mike Moore <mike@blowmage.com>
2014-02-06 05:49:20 -0800

    Add test for 1 + 1 != 0
    $ ruby test/getting_started/test_arithmetic.rb
    Run options: --seed 27380
    
    ..
    
    Finished tests in 0.000546s, 3663.0037 tests/s, 3663.0037 assertions/s.
    
    2 tests, 2 assertions, 0 failures, 0 errors, 0 skips

--- test/getting_started/test_arithmetic.rb
+++ test/getting_started/test_arithmetic.rb
@@ -4,4 +4,8 @@ class TestArithmetic < MiniTest::Unit::TestCase
   def test_one_plus_one_is_two
     assert_equal 2, 1 + 1
   end
+
+  def test_one_plus_one_isnt_zero
+    refute_equal 0, 1 + 1
+  end
 end
