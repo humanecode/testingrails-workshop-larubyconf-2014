b84c3442448a7f1f46a6d81164feff92a118201c
Mike Moore <mike@blowmage.com>
2014-02-06 14:51:05 -0800

    Add Membership is invalid without really unique group/user test

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,6 @@ class Membership < ActiveRecord::Base
   belongs_to :group
   belongs_to :user
 
-  validates :group, presence: true
-  validates :user, presence: true
+  validates :group_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :group_id }
 end
--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -25,12 +25,32 @@ class MembershipTest < ActiveSupport::TestCase
   def test_is_invalid_without_group
     membership.group = nil
     refute membership.valid?
-    assert_includes membership.errors[:group], "can't be blank"
+    assert_includes membership.errors[:group_id], "can't be blank"
   end
 
   def test_is_invalid_without_user
     membership.user = nil
     refute membership.valid?
-    assert_includes membership.errors[:user], "can't be blank"
+    assert_includes membership.errors[:user_id], "can't be blank"
+  end
+
+  def test_invalid_with_duplicate_group_user
+    membership = memberships :dev_member
+    membership2 = Membership.new
+    membership2.group = membership.group
+    membership2.user  = membership.user
+    refute membership2.valid?
+    assert_includes membership2.errors[:user_id], "has already been taken"
   end
 end
+
+
+
+
+
+
+
+
+
+
+
