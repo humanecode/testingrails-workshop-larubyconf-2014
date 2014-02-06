d0610481513120a7a0cf3f018c0caeec6e283dba
Mike Moore <mike@blowmage.com>
2014-02-06 05:42:18 -0800

    Add greeting with name test

--- test/getting_started/test_hello_world.rb
+++ test/getting_started/test_hello_world.rb
@@ -1,13 +1,17 @@
 require "minitest/autorun"
 
 class HelloWorld
-  def greet
-    "Hello world!"
+  def greet name = "world"
+    "Hello #{name}!"
   end
 end
 
 class HelloWorldTest < MiniTest::Unit::TestCase
   def test_greeting
     assert_equal "Hello world!", HelloWorld.new.greet
+  end
+
+  def test_greeting_with_name
+    assert_equal "Hello Mike!", HelloWorld.new.greet("Mike")
   end
 end
