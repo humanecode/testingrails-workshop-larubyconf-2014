3e434f22dd7bf167ba606b4819b28e63800be4cd
Mike Moore <mike@blowmage.com>
2014-02-06 13:30:58 -0800

    Add Group is invalid without name test
    $ rake test
    Run options: --seed 9937
    
    # Running tests:
    
    g....................
    
    Finished tests in 0.836187s, 23.9181 tests/s, 44.2485 assertions/s.
    
    20 tests, 37 assertions, 0 failures, 0 errors, 0 skips

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_can_be_valid
+    group = groups :dev
+    assert group.valid?
+  end
+
+  def test_invalid_without_name
+    group = groups :dev
+    group.name = nil
+    refute group.valid?
+    assert_includes group.errors[:name], "can't be blank"
+  end
 end
