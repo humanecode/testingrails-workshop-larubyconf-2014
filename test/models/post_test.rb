28125d3e508af27cd495afd6d87c681df4f8402d
Mike Moore <mike@blowmage.com>
2014-02-07 08:44:55 -0800

    Convert Post test to spec DSL

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,75 +1,69 @@
 require 'test_helper'
 
-class PostTest < ActiveSupport::TestCase
-  def group
-    @group ||= groups :dev
+describe Post do
+  let(:group) { groups :dev }
+  let(:user)   { users :member }
+  let(:day)   { Date.today }
+  let(:post) do
+    p = Post.new
+    p.group = group
+    p.user = user
+    p.date = day
+    p.body = "Great day!"
+    p
   end
 
-  def user
-    @user ||= users :member
-  end
-
-  def day
-    Date.today
-  end
-
-  def post
-    @post ||= begin
-      p = Post.new
-      p.group = group
-      p.user = user
-      p.date = day
-      p.body = "Great day!"
-      p
-    end
-  end
-
-  def test_can_be_valid
+  it "can be valid" do
     assert post.valid?
+    post.must_be :valid?
   end
 
-  def test_is_invalid_without_group
+  it "is invalid without a group" do
     post.group = nil
-    refute post.valid?
-    assert_includes post.errors[:group_id], "can't be blank"
+    post.wont_be :valid?
+    post.errors[:group_id].must_include "can't be blank"
   end
 
-  def test_is_invalid_without_user
+  it "is invalid without a user" do
     post.user = nil
-    refute post.valid?
-    assert_includes post.errors[:user_id], "can't be blank"
+    post.wont_be :valid?
+    post.errors[:user_id].must_include "can't be blank"
   end
 
-  def test_is_invalid_without_day
+  it "is invalid without a date" do
     post.date = nil
-    refute post.valid?
-    assert_includes post.errors[:date], "can't be blank"
+    post.wont_be :valid?
+    post.errors[:date].must_include "can't be blank"
   end
 
-  def test_is_invalid_without_day
+  it "is invalid without a body" do
     post.body = nil
-    refute post.valid?
-    assert_includes post.errors[:body], "can't be blank"
+    post.wont_be :valid?
+    post.errors[:body].must_include "can't be blank"
   end
 
-  def test_invalid_with_duplicate_group_user_date
+  it "is invalid with a duplicate group/user/date" do
     post = posts :today
     post2 = Post.new
     post2.group = post.group
     post2.user  = post.user
     post2.date  = post.date
     post2.body  = post.body
-    refute post2.valid?
-    assert_includes post2.errors[:user_id], "has already been taken"
+
+    post2.wont_be :valid?
+    post2.errors[:user_id].must_include "has already been taken"
   end
 
   # Add a new comment: Post#add_comment(user, body)
 
-  def test_can_add_a_new_comment
+  it "can add a new comment" do
     post = posts :today
     user = users :nonmember
     assert_difference "Comment.count" do
       post.add_comment user, "New Comment!"
     end
+    # lambda do
+    #   post.add_comment user, "New Comment!"
+    # end.must_change "New Comment!"
   end
 end
